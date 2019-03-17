a1ee12b548bdcc1474011452232d9566e21d263f
Your Name <you@example.com>
2019-03-17 12:45:11 +0000

    Encoding User, Deserialize User, Enabling Cookies

--- index.js
+++ index.js
@@ -1,15 +1,26 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cookieSession = require('cookie-session');
+const passport = require('passport');
 const keys = require('./config/keys');
 require('./models/User');
 require('./services/passport');
 
-mongoose.connect(keys.mongoURI);
+mongoose.connect(keys.mongoURI, { useNewUrlParser: true });
 
 const app = express();
 
+app.use(
+    cookieSession({
+        maxAge: 24 * 60 * 60 * 1000,
+        keys: [keys.cookieKey]
+    })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
+
 require('./routes/authRoutes')(app);
 
-const port = process.env.PORT || 5000;
-app.listen(port, () => console.log(`App is running on port ${port}`));
-
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, console.log(`Server running on PORT ${PORT}`));
