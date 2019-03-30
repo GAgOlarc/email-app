1bb1d4f61adba55201d4e1c762c28ece425f2445
Your Name <you@example.com>
2019-03-30 11:54:48 +0000

    Landing Component & Link Tags

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 
 class Header extends Component {
     renderContent = () => {
@@ -21,7 +22,12 @@ class Header extends Component {
         return (
             <nav>
                 <div className="nav-wrapper">
-                    <a href="/" className="left brand-logo">Email-app</a>
+                    <Link
+                        to={this.props.auth ? '/surveys' : '/'}
+                        className="left brand-logo"
+                    >
+                        Email-app
+                    </Link>
                     <ul id="nav-mobile" className="right hide-on-small-and-down">
                         {this.renderContent()}
                     </ul>
