efc92c7da1be1ec715e3e380adc5a6744725a895
Your Name <you@example.com>
2019-04-09 20:51:51 +0100

    Add Survey Fields

---
+++ client/src/components/surveys/SurveyForm.js
@@ -0,0 +1,48 @@
+import React, { Component } from 'react';
+import { reduxForm, Field } from 'redux-form';
+import { Link } from 'react-router-dom';
+import SurveyField from './SurveyField';
+
+const FIELDS = [
+    { name: "title", label: "Survey Title" },
+    { name: "subject", label: "Subject Line" },
+    { name: "body", label: "Email Body" },
+    { name: "emails", label: "Recipient List" }
+];
+
+class SurveyForm extends Component {
+    renderFields() {
+        return FIELDS.map(({ name, label }) => (
+            <Field
+                key={name}
+                type="text"
+                name={name}
+                label={label}
+                component={SurveyField}
+            />
+        ));
+    }
+
+    render() {
+        return (
+            <div>
+                <form
+                    onSubmit={this.props.handleSubmit(values => console.log(values))}
+                >
+                    {this.renderFields()}
+                    <Link to="/surveys" className="red btn-flat white-text">
+                        Cancel
+                    </Link>
+                    <button type="submit" className="teal btn-flat right white-text">
+                        Next
+                        <i className="material-icons right">done</i>
+                    </button>
+                </form>
+            </div>
+        )
+    }
+}
+
+export default reduxForm({
+    form: 'surveyForm'
+})(SurveyForm);
