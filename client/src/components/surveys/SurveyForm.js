0c4c51149e0c35645bb2312618218ea131a59b55
Your Name <you@example.com>
2019-04-11 21:12:08 +0100

    Displaying Form Fields on Review Page

--- client/src/components/surveys/SurveyForm.js
+++ client/src/components/surveys/SurveyForm.js
@@ -3,17 +3,11 @@ import { reduxForm, Field } from 'redux-form';
 import { Link } from 'react-router-dom';
 import SurveyField from './SurveyField';
 import validateEmails from '../../utils/validateEmails';
-
-const FIELDS = [
-    { name: "title", label: "Survey Title" },
-    { name: "subject", label: "Subject Line" },
-    { name: "body", label: "Email Body" },
-    { name: "emails", label: "Recipient List" }
-];
+import formFields from './formFields';
 
 class SurveyForm extends Component {
     renderFields() {
-        return FIELDS.map(({ name, label }) => (
+        return formFields.map(({ name, label }) => (
             <Field
                 key={name}
                 type="text"
@@ -28,7 +22,7 @@ class SurveyForm extends Component {
         return (
             <div>
                 <form
-                    onSubmit={this.props.handleSubmit(values => console.log(values))}
+                    onSubmit={this.props.handleSubmit(this.props.onSurveySubmit)}
                 >
                     {this.renderFields()}
                     <Link to="/surveys" className="red btn-flat white-text">
@@ -49,7 +43,7 @@ function validate(values) {
 
     errors.emails = validateEmails(values.emails || '');
 
-    FIELDS.forEach(({ name }) => {
+    formFields.forEach(({ name }) => {
         if (!values[name]) {
             errors[name] = `You must provide a value`;
         }
@@ -59,6 +53,7 @@ function validate(values) {
 }
 
 export default reduxForm({
+    validate,
     form: 'surveyForm',
-    validate
+    destroyOnUnmount: false
 })(SurveyForm);
--- client/src/components/surveys/SurveyNew.js
+++ client/src/components/surveys/SurveyNew.js
@@ -1,11 +1,28 @@
 import React, { Component } from 'react';
 import SurveyForm from './SurveyForm';
+import SurveyFormReview from './SurveyFormReview';
 
 class SurveyNew extends Component {
+    state = { showFormReview: false };
+
+    // renderContent() {
+    //     if (this.state.showFormReview) {
+    //         return <SurveyFormReview />
+    //     }
+
+    //     return <SurveyForm
+    //         onSurveySubmit={() => this.setState({ showFormReview: true })}
+    //     />
+    // }
+
     render() {
+        const renderContent = this.state.showFormReview
+            ? <SurveyFormReview onCancel={() => this.setState({ showFormReview: false })} />
+            : <SurveyForm onSurveySubmit={() => this.setState({ showFormReview: true })} />
+
         return (
             <div>
-                <SurveyForm />
+                {renderContent}
             </div>
         )
     }
