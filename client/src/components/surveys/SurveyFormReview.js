d64defad2a16a8492e819e5f317d53a014a2d15a
Your Name <you@example.com>
2019-04-12 18:29:45 +0100

    Posting and redirect to surveys on submit.

--- client/src/components/surveys/SurveyFormReview.js
+++ client/src/components/surveys/SurveyFormReview.js
@@ -1,10 +1,11 @@
 // SurveyFormReview shows users their form inputs for review
 import React from 'react';
 import { connect } from 'react-redux';
+import { withRouter } from 'react-router-dom';
 import formFields from './formFields';
 import * as actions from '../../actions/index';
 
-const SurveyFormReview = ({ onCancel, formValues, submitSurvey }) => {
+const SurveyFormReview = ({ onCancel, formValues, submitSurvey, history }) => {
     const reviewFields = formFields.map(({ name, label }) => (
         <div key={name}>
             <label>{label}</label>
@@ -24,7 +25,7 @@ const SurveyFormReview = ({ onCancel, formValues, submitSurvey }) => {
             </button>
             <button
                 className="green btn-flat right white-text"
-                onClick={() => submitSurvey(formValues)}
+                onClick={() => submitSurvey(formValues, history)}
             >
                 Send Survey
                 <i className="material-icons right">email</i>
@@ -37,4 +38,4 @@ const mapStateToProps = state => ({
     formValues: state.form.surveyForm.values
 });
 
-export default connect(mapStateToProps, actions)(SurveyFormReview);
+export default connect(mapStateToProps, actions)(withRouter(SurveyFormReview));
